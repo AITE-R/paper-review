cf1a5f29d6dd3289fe481d81b09708e52993b9bf
Sangh0 <hop7311@naver.com>
2024-02-19 00:04:43 +0900

    feat: add embedding modules

---
+++ Transformer/model/embedding.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+
+class TokenEmbedding(nn.Embedding):
+    def __init__(self, vocab_size: int, d_model: int) -> None:
+        super(TokenEmbedding, self).__init__(vocab_size, d_model, padding_idx=1)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, max_len: int, device: str) -> None:
+        super(PositionalEncoding, self).__init__()
+
+        self.encoding = torch.zeros(max_len, d_model, device=device)
+        self.encoding.requires_grad = False
+
+        pos = torch.arange(0, max_len, device=device)
+        pos = pos.float().unsqueeze(dim=1)
+
+        _2i = torch.arange(0, d_model, step=2, device=device).float()
+
+        self.encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
+        self.encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        _, seq_len = x.size()
+        return self.encoding[:seq_len, :]
+
+
+class TransformerEmbedding(nn.Module):
+    def __init__(
+        self, vocab_size: int, d_model: int, max_len: int, drop_prob: float, device: str
+    ) -> None:
+        super(TransformerEmbedding, self).__init__()
+        self.tok_emb = TokenEmbedding(vocab_size, d_model)
+        self.pos_emb = PositionalEncoding(d_model, max_len, device)
+        self.drop_out = nn.Dropout(p=drop_prob)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        tok_emb = self.tok_emb(x)
+        pos_emb = self.pos_emb(x)
+        return self.drop_out(tok_emb + pos_emb)
